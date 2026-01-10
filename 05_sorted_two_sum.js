b0237d063d4504a5a69918b2f70a2c9580f2485a
devAO-bit <abhitech.work@gmail.com>
2026-01-10 22:06:10 +0530

    Add twoSum function to find indices of numbers that sum to a target in a sorted array

---
+++ 05_sorted_two_sum.js
@@ -0,0 +1,24 @@
+function twoSum(numbers, target) {
+    let left = 0;
+    let right = numbers.length - 1;
+
+    while(left < right) {
+        const sum = numbers[left] + numbers[right]
+
+        if(sum === target) {
+            return [left + 1, right + 1]
+        }
+
+        if(sum < target) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+    return [];
+}
+
+let numbers = [2, 7, 11, 15]
+let target = 9
+
+console.log(twoSum(numbers, target))
