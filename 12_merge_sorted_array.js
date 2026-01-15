41f25c171a7ff41b95d5aabf8d3bed5c867124d9
devAO-bit <abhitech.work@gmail.com>
2026-01-15 22:15:18 +0530

    feat: Implement merge function to merge two sorted arrays

---
+++ 12_merge_sorted_array.js
@@ -0,0 +1,33 @@
+function merge(nums1, m, nums2, n) {
+    let i = m - 1;
+    let j = n - 1;
+    let k = m + n - 1;
+
+    // Merge from the back
+    while (i >= 0 && j >= 0) {
+        if (nums1[i] > nums2[j]) {
+            nums1[k] = nums1[i];
+            i--;
+        } else {
+            nums1[k] = nums2[j];
+            j--;
+        }
+        k--;
+    }
+
+    // Copy remaining elements of nums2 (if any)
+    while (j >= 0) {
+        nums1[k] = nums2[j];
+        j--;
+        k--;
+    }
+
+    return nums1;
+}
+
+let nums1 = [1,2,3,0,0,0]
+let m = 3
+let nums2 = [2,5,6]
+let n = 3
+
+console.log(merge(nums1, m, nums2, n))
