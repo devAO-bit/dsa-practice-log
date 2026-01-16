bf6868c1423e598e2756da3f8abbb480a045ae67
devAO-bit <abhitech.work@gmail.com>
2026-01-16 22:17:17 +0530

    feat: Implement isPalindrome function to check if a string is a palindrome

---
+++ 14_valid_palindrome.js
@@ -0,0 +1,21 @@
+function isPalindrome(s) {
+    let left = 0;
+    let right = s.length - 1;
+
+    while(left < right) {
+        while(left < right && !isAlphaNum(s[left])) left++;
+        while(left < right && !isAlphaNum(s[right])) right--;
+
+        if(s[left].toLowerCase() !== s[right].toLowerCase()) return false;  
+
+        left++;
+        right--;
+    }
+    return true;
+}
+
+function isAlphaNum(char) {
+    return /^[a-z0-9]$/i.test(char);
+}
+
+console.log(isPalindrome("A man, a plan, a canal: Panama"))
