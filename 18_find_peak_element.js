c047ae340256bc79ccfc24f80a727fb76562a5fb
devAO-bit <abhitech.work@gmail.com>
2026-01-20 22:20:58 +0530

    feat: Add initial project configuration files and implement findPeakElement function

---
+++ 18_find_peak_element.js
@@ -0,0 +1,17 @@
+var findPeakElement = function(nums) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while(left < right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if(nums[mid] < nums[mid + 1]) {
+            left = mid + 1
+        } else {
+            right = mid
+        }
+    }
+    return left;
+}
+
+console.log(findPeakElement([1, 2, 3, 1]))
