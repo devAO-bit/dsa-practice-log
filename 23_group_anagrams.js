e37bb9e1959b96fc4bcc03168c6e9e53230ffc75
devAO-bit <abhitech.work@gmail.com>
2026-01-25 23:34:38 +0530

    feat: Implementing group anagrams

---
+++ 23_group_anagrams.js
@@ -0,0 +1,24 @@
+var groupAnagrams = function(strs) {
+    let map = new Map();
+
+    for (let word of strs) {
+        let count = new Array(26).fill(0);
+
+        for (let char of word) {
+            count[char.charCodeAt(0) - 97]++;
+        }
+
+        let key = count.join('#');
+
+        if (!map.has(key)) {
+            map.set(key, []);
+        }
+        map.get(key).push(word);
+    }
+
+    return Array.from(map.values());
+};
+
+let strs = ["eat","tea","tan","ate","nat","bat"]
+
+console.log(groupAnagrams(strs))
