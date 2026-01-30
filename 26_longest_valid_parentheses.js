ff1ce0643984c793a05b4fdcc1df331091031a92
devAO-bit <abhitech.work@gmail.com>
2026-01-30 09:32:01 +0530

    Ifeat: implemented the longest valid parentheses problem

---
+++ 26_longest_valid_parentheses.js
@@ -0,0 +1,23 @@
+var longestValidParentheses = function(s) {
+    let stack = [];
+    stack.push(-1);
+
+    let maxLen = 0;
+
+    for(let i = 0; i < s.length; i++) {
+        if(s[i] === '(') {
+            stack.push(i)
+        } else {
+            stack.pop();
+
+            if(stack.length === 0) {
+                stack.push(i)
+            } else {
+                maxLen = Math.max(maxLen, i - stack[stack.length - 1])
+            }
+        }
+    }
+    return maxLen;
+}
+
+console.log(longestValidParentheses(')()()('))
