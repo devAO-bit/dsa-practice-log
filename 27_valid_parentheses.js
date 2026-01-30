db910bf42dbc1a38ae44d6e75d771d6df16a814b
devAO-bit <abhitech.work@gmail.com>
2026-01-30 09:38:09 +0530

    feat: implemented is valid parentheses solution

---
+++ 27_valid_parentheses.js
@@ -0,0 +1,23 @@
+var isValid = function(s) {
+    let stack = [];
+    let map = {
+        ')' : '(',
+        '}' : '{',
+        ']' : '['
+    }
+
+    for(let char of s) {
+        if(char === '(' || char === '{' || char === '[') {
+            stack.push(char)
+        } else {
+            if(stack.length === 0) return false
+
+            let top = stack.pop();
+
+            if(top !== map[char]) return false;
+        }
+    }
+    return stack.length === 0;
+}
+
+console.log(isValid('(){}['))
