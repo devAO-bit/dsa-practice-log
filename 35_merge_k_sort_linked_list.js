02bc4a0b9043d9c5024a31d1060ff440d528711a
devAO-bit <abhitech.work@gmail.com>
2026-02-08 18:42:17 +0530

    feat: Implemented merge k sort linked list

---
+++ 35_merge_k_sort_linked_list.js
@@ -0,0 +1,69 @@
+console.log("Start")
+
+function ListNode(val, next = null) {
+    this.val = val;
+    this.next = next;
+}
+
+function arrayToLinkedList(arr) {
+    let head = new ListNode(arr[0])
+    let curr = head
+
+    for(let i = 1; i < arr.length; i++) {
+        curr.next = new ListNode(arr[i])
+        curr = curr.next
+    }
+    return head
+}
+
+var mergeTwoLists = function(l1, l2) {
+    let dummy = new ListNode(0)
+    let tail = dummy
+
+    while(l1 !== null && l2  !== null) {
+        if(l1.val < l2.val) {
+            tail.next = l1
+            l1 = l1.next;
+        } else {
+            tail.next = l2
+            l2 = l2.next;
+        }
+        tail = tail.next
+    }
+    tail.next = l1 !== null ? l1 : l2
+
+    return dummy.next
+}
+
+var mergeKLists = function(lists) {
+    if(!lists || lists.length === 0) return null
+
+    while(lists.length > 1) {
+        let mergedLists = []
+
+        for(let i = 0; i < lists.length; i += 2) {
+            let l1 = lists[i];
+            let l2 = (i + 1 < lists.length) ? lists[i+1] : null;
+            mergedLists.push(mergeTwoLists(l1, l2))
+        }
+        lists = mergedLists
+    }
+     return lists[0];   
+}
+
+function printList(head) {
+    let result = [];
+    while (head !== null) {
+        result.push(head.val);
+        head = head.next;
+    }
+    console.log(result.join(" -> "));
+}
+
+
+let l1 = arrayToLinkedList([1,4,5]);
+let l2 = arrayToLinkedList([1,3,4]);
+let l3 = arrayToLinkedList([2,6]);
+
+let result = mergeKLists([l1, l2, l3]);
+printList(result);
