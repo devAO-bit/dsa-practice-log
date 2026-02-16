6cea76faf8bb1e99ef0cf007986df609eb13cb9c
devAO-bit <abhitech.work@gmail.com>
2026-02-16 15:04:30 +0530

    feat(js): Implemented permutation using used[] array

---
+++ 46_permutation.js
@@ -0,0 +1,27 @@
+function permute(nums) {
+    let result = []
+    let used = new Array(nums.length).fill(false)
+
+    function backtrack(path) {
+        if(path.length === nums.length) {
+            result.push([...path])
+            return;
+        }
+
+        for(let i = 0; i < nums.length; i++) {
+            if(used[i]) continue
+
+            used[i] = true
+            path.push(nums[i])
+
+            backtrack(path)
+
+            path.pop()
+            used[i] = false
+        } 
+    }
+    backtrack([])
+    return result;
+}
+
+console.log(permute([1, 2, 3]))
