87e26520c1aacf5a48d88769876c5cddc6852deb
devAO-bit <abhitech.work@gmail.com>
2026-02-17 23:24:23 +0530

    feat(js): Implemented the letter combination of phone numbers

---
+++ 49_letter_combination_phone_number.js
@@ -0,0 +1,33 @@
+function letterCombination(digits) {
+    if(!digits.length) return []
+
+    const map = {
+        2: "abc",
+        3: "def",
+        4: "ghi",
+        5: "jkl",
+        6: "mno",
+        7: "pqrs",
+        8: "tuv",
+        9: "wxyz"
+    };
+
+    let result = [];
+
+    function backtrack(index, path) {
+        if(index === digits.length) {
+            result.push(path);
+            return;
+        }
+
+        let letters = map[digits[index]]
+
+        for(let char of letters) {
+            backtrack(index + 1, path + char)
+        }
+    }
+    backtrack(0, "");
+    return result;
+}
+
+console.log(letterCombination("23"))
