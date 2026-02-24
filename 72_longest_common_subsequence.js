142b40651ab00714e138c4ff15e7c006c224fae4
devAO-bit <abhitech.work@gmail.com>
2026-02-24 13:18:13 +0530

    feat: Implemented the longest common subsequence problem

---
+++ 72_longest_common_subsequence.js
@@ -0,0 +1,22 @@
+var longestCommonSubsequence = function(text1, text2) {
+    let m = text1.length
+    let n = text2.length
+
+    let dp = Array(m + 1).fill(0).map(() => Array(n + 1).fill(0))
+
+    for(let i = 1; i <= m; i++){
+        for(let j = 1; j <= n; j++) {
+            if(text1[i - 1] === text2[j - 1]) {
+                dp[i][j] = 1 + dp[i - 1][j - 1]
+            } else {
+                dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1])
+            }
+        }
+    }
+    return dp[m][n]
+}
+
+let text1 = "abcd"
+let text2 = "abc"
+
+console.log(longestCommonSubsequence(text1, text2))
