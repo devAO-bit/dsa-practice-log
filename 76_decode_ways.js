8a7f3f4c9b61b162f21ba0dc1b0cd093dca749c3
devAO-bit <abhitech.work@gmail.com>
2026-02-25 20:09:06 +0530

    Feat: implemented the bottom up dp solution of nums decoding

---
+++ 76_decode_ways.js
@@ -0,0 +1,23 @@
+var numDecodings = function(s) {
+    if(s[0] === '0') return 0;
+
+    let dp = new Array(s.length + 1).fill(0)
+
+    dp[0] = 1
+    dp[1] = 1
+
+    for(let i = 2; i <= s.length; i++) {
+        if(s[i - 1] !== '0') {
+            dp[i] += dp[i - 1]
+        }
+
+        let twoDigit = parseInt(s.substring(i - 2, i))
+        if(twoDigit >= 10 && twoDigit <= 26){
+            dp[i] += dp[i - 2]
+        } 
+    }
+    return dp[s.length]
+}
+
+let s = '226'
+console.log(numDecodings(s))
