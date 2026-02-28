ddcb96ef5c2ae70a0a184a69f2ec1fdea2082547
devAO-bit <abhitech.work@gmail.com>
2026-02-28 20:04:06 +0530

    feat: Implemented the uninque path II

---
+++ 80_unique_path_2.js
@@ -0,0 +1,40 @@
+var uniquePathsWithObstacles = function(obstacleGrid) {
+    let m = obstacleGrid.length;
+    let n = obstacleGrid[0].length;
+
+    if (obstacleGrid[0][0] === 1) return 0;
+
+    let dp = Array.from({length: m}, () => new Array(n).fill(0));
+
+    dp[0][0] = 1;
+
+    // First column
+    for (let i = 1; i < m; i++) {
+        dp[i][0] = obstacleGrid[i][0] === 0 ? dp[i-1][0] : 0;
+    }
+
+    // First row
+    for (let j = 1; j < n; j++) {
+        dp[0][j] = obstacleGrid[0][j] === 0 ? dp[0][j-1] : 0;
+    }
+
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            if (obstacleGrid[i][j] === 1) {
+                dp[i][j] = 0;
+            } else {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+    }
+
+    return dp[m-1][n-1];
+};
+let obstacleGrid =
+[
+  [0,0,0],
+  [0,1,0],
+  [0,0,0]
+]
+
+console.log(uniquePathsWithObstacles(obstacleGrid))
